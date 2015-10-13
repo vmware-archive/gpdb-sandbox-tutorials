d7af939f0d1d3cc8937fea06146502ae3ea65d6b
dbbaskette <dbbaskette@gmail.com>
2015-10-13 19:13:35 -0400

    web table

--- ext_web_tables.sql
+++ ext_web_tables.sql
@@ -1,14 +1,4 @@
-DROP TABLE IF EXISTS WWearthquakes_lastWk;
-CREATE TABLE WWearthquakes_lastWk (
-time TEXT, latitude numeric, longitude numeric, depth numeric, mag numeric, mag_type varchar (10),
-NST integer, gap numeric, dmin numeric, rms text, net text, id text, updated TEXT, place varchar(150), type varchar(50)
-)
-DISTRIBUTED BY (time);
-
-DROP EXTERNAL TABLE IF EXISTS ext_WWearthquakes_lastWk;
-create external web table ext_WWearthquakes_lastWk (like WWearthquakes_lastWk) 
-Execute 'wget -qO - http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.csv'  -- defining an OS command to execute
-ON ALL
-Format 'CSV' (HEADER)
-LOG ERRORS INTO err_earthquakes
-Segment Reject limit 300;
+DROP EXTERNAL TABLE IF EXISTS ext_weather;
+create external web table ext_weather (like weather)
+Execute 'wget -qO - https://github.com/Pivotal-Open-Source-Hub/gpdb-sandbox-tutorials/blob/master/data/weather_2013.csv?raw=true' on all format 'csv' (header) 
+LOG ERRORS INTO err_weather segment reject limit 100;
